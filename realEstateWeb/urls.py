b12970bd5218e9ac49938a931dc3df367c27625a
S4NKALP <sankalptharu50028@gmail.com>
2025-07-09 14:15:04 +0545

    🔧 chore(urls): update media serve view and adjust path in DEBUG mode

--- realEstateWeb/urls.py
+++ realEstateWeb/urls.py
@@ -14,22 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.urls import include, re_path
+
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve as dev_serve_media
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
-    path('api/', include('app.urls')),
+    path("api/", include("app.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', dev_serve_media, name='media'),
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+        # re_path(r'^media/(?P<path>.*)$', serve, name='media'),
     ]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += [
-    path('', admin.site.urls),
+    path("", admin.site.urls),
 ]
